89421ddbb782f2adbccbdc0efb913bea1f6341f0
Anatolii <feelmysmell@yandex.ru>
2023-02-03 23:29:48 +0500

    Домашняя 2

---
+++ index.js
@@ -0,0 +1,28 @@
+// Задача 1
+// let num1 = parseInt(prompt("Введите ваше первое число"));
+// let num2 = parseInt(prompt("Введите ваше второе число"));
+// let result1 = (num1 === 1 || num1 < 1) ? alert(`${num1} равно 1 или меньше`) : alert("Условие один не выполняется")
+// let result2 = (num2 === 3 || num1 > 3) ? alert(`${num2} равно 3 или меньше`) : alert("Условие два не выполняется")
+
+// Задача 2
+// let test = false;
+// let result = (test === true) ? console.log("+++") : console.log("---")
+
+// Задача 3
+// let day = Math.floor(Math.random() * 31) + 1;
+// if (day <= 10) {
+//     alert(`${day} первая декада`)
+// } else if (day > 10 && day < 21) {
+//     alert(`${day} вторая декада`)
+// } else {
+//     alert(`${day} третья декада`)
+// }
+
+// Задача 4
+// let userNumber = parseInt(prompt("Введите число"));
+
+// let one = userNumber % 10; // единицы
+// let two = Math.trunc(( userNumber / 10 ) % 10); // десятки
+// let three = parseInt( userNumber / 100 ) ; // сотни
+
+// console.log(`В числе ${userNumber} кол-во единиц ${one}, кол-во десятков ${two}, кол-во сотен ${three}`)
